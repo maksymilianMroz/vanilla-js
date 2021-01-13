79c4ece3dfb7f6dcb324cbc15666668151bf386b
maksymilianMroz <mrozmaksymilian@gmail.com>
2021-01-13 01:13:55 +0100

    Function which will count the score for numbers 3+ done

--- fibonacci-sequence/script.js
+++ fibonacci-sequence/script.js
@@ -2,6 +2,15 @@ let n1 = 1;
 let n2 = 1;
 let n3;
 
+const fibonacci = (n) => {
+    for (let i = 2; i < n; i++) {
+        n3 = n2 + n1;
+        n1 = n2;
+        n2 = n3;
+    }
+    console.log(n3);
+}
+
 const fibb = (number) => {
     if (number <= 0) return console.log('Let\'s start from 1+');
     number <= 2 ? console.log(1) : console.log('nope');
