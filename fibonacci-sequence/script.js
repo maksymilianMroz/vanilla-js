0610619550493c9cae561b9365490697a48a434d
maksymilianMroz <mrozmaksymilian@gmail.com>
2021-01-13 01:11:46 +0100

    If the number is lower/eq 0 write smth in console, if its 1 or 2 score is 1

--- fibonacci-sequence/script.js
+++ fibonacci-sequence/script.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+let n1 = 1;
+let n2 = 1;
+let n3;
+
+const fibb = (number) => {
+    if (number <= 0) return console.log('Let\'s start from 1+');
+    number <= 2 ? console.log(1) : console.log('nope');
+};
+
+fibb(0);
