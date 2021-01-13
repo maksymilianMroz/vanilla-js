99bde5549f3d0f86b89732e5a811925a029e5c0d
maksymilianMroz <mrozmaksymilian@gmail.com>
2021-01-14 00:21:21 +0100

    Fibonacci counter is working, but showing whole array with all numbers, and if i want specific number, ive got undifined

--- fibonacci-sequence/script.js
+++ fibonacci-sequence/script.js
@@ -22,3 +22,10 @@
 //Another version of fibonacci sequence - with arrays
 
 let arr = [0, 1];
+const fib = (n) => {
+    for (let i = 2; i < n; i++) {
+        arr.push(arr[i-2] + arr[i-1]);
+    }
+    console.log(arr);
+};
+fib(6);
